8fe39c1ecc2fcf73597099d2a04c4753a84cc8d0
Victor Shamanov <wiruzx@gmail.com>
2015-08-11 23:24:50 +0300

    Refactor MESegmentControl to use MEBadgeFactory

--- MESegmentedControl/MESegmentedControl.h
+++ MESegmentedControl/MESegmentedControl.h
@@ -5,20 +5,24 @@
 //  Copyright (c) 2012 David Thompson. All rights reserved.
 //
 
+#import "MEBadgeFactoryType.h"
+
 @class CustomBadge;
 
-@interface MESegmentedControl : UISegmentedControl
-{
+@interface MESegmentedControl : UISegmentedControl {
 @private
 	NSMutableArray *_segmentBadgeNumbers;
 	NSMutableArray *_segmentBadges;
     UIView *_badgeView;
 }
 
+// By default is MEBadgeFactory
+@property (nonatomic, strong) id <MEBadgeFactoryType> badgeFactory;
+
 // Set the badge number for a specific segment.
 // Setting the badge number to 0 will clear the badge for that segment.
 // Use the block to make adjustments to badge formatting, using the methods outlined in CustomBadge.h.
-- (void)setBadgeNumber:(NSUInteger)badgeNumber forSegmentAtIndex:(NSUInteger)segmentIndex usingBlock:(void(^)(CustomBadge *))configureBadge;
+- (void)setBadgeNumber:(NSUInteger)badgeNumber forSegmentAtIndex:(NSUInteger)segmentIndex usingBlock:(void(^)(id <MEBadgeType>))configureBadge;
 
 // Convenience method for setting a badge number with default look and feel.
 - (void)setBadgeNumber:(NSUInteger)badgeNumber forSegmentAtIndex:(NSUInteger)segmentIndex;
