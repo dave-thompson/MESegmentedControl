4ce93a2318deb0f15b0a4a375cdca5de29d16212
Victor Shamanov <wiruzx@gmail.com>
2015-08-11 23:10:58 +0300

    Add MEBadgeFactory

---
+++ MESegmentedControl/MEBadgeFactory.h
@@ -0,0 +1,15 @@
+//
+//  MEBadgeFactory.h
+//  
+//
+//  Created by Victor Shamanov on 8/11/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+#import "MEBadgeFactoryType.h"
+
+@interface MEBadgeFactory : NSObject <MEBadgeFactoryType>
+
+@end
